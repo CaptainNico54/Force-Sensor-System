9df973ed1da78efd02c2bd61640b96422aad1560
MakerMatrix <agentsmith@makermatrix.com>
2021-07-11 22:26:20 -0500

    Cleaning setup.h

--- ForceSensorGraph/include/setup.h
+++ ForceSensorGraph/include/setup.h
@@ -1,20 +1,15 @@
-/*
-NOTE: the constructor for the TFT object uses hardwired values for the SPI pins.
+// Which version of the PCB do you have?
+//#define PCBV1
+#define PCBV2
+
+/*   #############   IMPORTANT - ACTION REQUIRED!!!!   ##############
+The constructor for the TFT_ILI9341 object uses hardwired values for the SPI pins.
 So, currently these must be set in the User_Setup.h file that comes with the library.
 For the latest board (PCBV2), TFT_DC == 8; TFT_RST == 9; TFT_CS == 10.
 For the older board (PCBV1), TFT_DC == SDA; TFT_RST == -1; TFT_CS == SCL.
 ALSO: You must comment out FAST_LINE, and all the fonts except for LOAD_GLCD
 Look for this file in the folder .pio/libdeps/<platform>/TFT_ILI9341
 */
-
-// If DEBUG is anything but zero, the program will block until a serial monitor is attached/open
-// Set to 1 to get force sensor values on the serial monitor
-// Set to 2 to get more detailed program status
-#define DEBUG 2
-
-// Which version of the PCB do you have?
-//#define PCBV1
-#define PCBV2
 
 #ifdef PCBV1
     #define HX711_DOUT A1
@@ -28,6 +23,11 @@ Look for this file in the folder .pio/libdeps/<platform>/TFT_ILI9341
     #define TARE_PIN 13
     #define LCD_SPI_EN 5 // Drive LCD_SPI_EN low to enable the 5V -> 3.3V logic converters
 #endif
+
+// If DEBUG is anything but zero, the program will block until a serial monitor is attached/open
+// Set to 1 to get force sensor values on the serial monitor
+// Set to 2 to get more detailed program status
+#define DEBUG 2
 
 // Screen orientation - set to true to invert the LCD
 const boolean flipScreen = false;
